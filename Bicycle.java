115ff38250e1937898c7f8a28e4ed88534357972
sighini <sighini@gmail.com>
2018-05-16 15:25:31 -0500

    Update Bicycle.java

--- Bicycle.java
+++ Bicycle.java
@@ -9,42 +9,30 @@ class Bicycle {
 		final float ratio = 1.6f;
 		int f_init_value = 38;
 		int r_init_value = 28;
-		int f_start_index = 0;
-		int r_start_index = 0;
+		
+		float init_ratio = ((float)f_init_value)/r_init_value;
 		int count = 0;
-		
-		//Get front index
-		for (int i = 0; i < f_cogs.length; i++) {
-			if (f_cogs[i] == f_init_value) {
-				f_start_index = i;
-			}
-		}
-		
-		//Get rear index
-		for (int i = 0; i < r_cogs.length; i++) {
-			if (r_cogs[i] == r_init_value) {
-				r_start_index = i;
-			}
-		}
 		
 		DecimalFormat df = new DecimalFormat("#.#");
 		df.setRoundingMode(RoundingMode.CEILING);
-		//df.setRoundingMode(RoundingMode.DOWN);
-		for (int i = f_start_index; i < f_cogs.length; i++) {
-			for (int j=r_start_index; j < r_cogs.length; j++) {
-				count++;
+		for (int i = 0; i < f_cogs.length; i++) {
+			for (int j = 0; j < r_cogs.length; j++) {
 				float r = ((float) f_cogs[i] )/r_cogs[j];
-				System.out.println(count + " - F: " + f_cogs[i] + " R: " + r_cogs[j] + " Ratio " + new DecimalFormat("#.###").format(r));
-
-				//Hack..not recommended
-				if (i == f_start_index) {
-					j = r_cogs.length;
-				}
-				//Hack end
 				
-				if (ratio == Float.parseFloat(df.format(r))) {
-					break;
+				if (init_ratio < ratio) {
+					if (r < ratio) {
+						count++;
+						System.out.println(count + " - F: " + f_cogs[i] + " R: " + r_cogs[j] + " Ratio " + new DecimalFormat("#.###").format(r));
+					}	
+				} else {
+					if (r > ratio) {
+						count++;
+						System.out.println(count + " - F: " + f_cogs[i] + " R: " + r_cogs[j] + " Ratio " + new DecimalFormat("#.###").format(r));
+					}	
 				}
+				
+
 			}
 		}
+	}
 }
